9e1e8058d11a52f1c9e3156230bfbf0a328f3467
Conklaven <jacob.klaven@gmail.com>
2022-01-24 15:45:15 +0200

    clickable boxes

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ const SearchBox = (props =>{
     return(
         <>
         <nav className="nav">
-        Search <input type='text' onChange={props.searchItem}/>
+        Search <input id="searchbox" type='text' onChange={props.searchItem}/>
     <img id="logo" src="https://1kur9t3xffe11yy9in1cqsuu-wpengine.netdna-ssl.com/wp-content/uploads/2020/08/cropped-Kenes-tours_Logo-WHITE-3-2048x803.png" height= "35px"/>
         </nav>
         </>
